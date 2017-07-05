4ea4a3b2b02016c73d034e074521aad7efc2d102
mahananda9889 <mahanandaray9889@gmail.com>
2017-07-05 10:30:32 +0600

    class-1

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        $(".menu-trigger").on('click', function() {
+            $(".off-canvas-menu").addClass("show-offcanvas-menu");
+        });
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
